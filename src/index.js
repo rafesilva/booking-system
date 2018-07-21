05861c922c7d5689574318d68fb7092091c87ba0
Rafael FS <rafesilva@gmail.com>
2018-07-21 16:15:23 +1000

    HTTP -> HTTPS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+import { createStore } from "redux";
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "ADD":
+			break;
+		case "SUBSTRACT":
+			break;
+	}
+return state;
+};
+
+
+const store = createStore(reducer, 1);
